7866ce7f4d5a2d8fe8d2fa62f6ce2cc3301da406
Thuan Nguyen <ninhthuan82us@gmail.com>
2017-08-23 17:58:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ bot = telegram.Bot(token='375084271:AAEguAYJ51fb4HxooL3a7RJp_vGUJ9xR0xY')
 @app.route('/HOOK', methods=['POST'])
 def webhook_handler():
     if request.method == "POST":
+        print request
         # retrieve the message in JSON and then transform it to Telegram object
         update = telegram.Update.de_json(request.get_json(force=True))
 
